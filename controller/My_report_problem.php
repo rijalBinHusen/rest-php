4e592125d42de5808d95aa3a4fdce2397db2b911
rijalBinHusen <jeryzal333@gmail.com>
2022-12-22 14:56:08 +0700

    update problem by id

--- controller/My_report_problem.php
+++ controller/My_report_problem.php
@@ -23,7 +23,7 @@ class My_report_problem
         $item = $req->query->item;
         // sennd data tomodel and accept the result
         if(!is_null($status)) {
-            $this->result_from_model = $this->my_report_problem->get_problem_actives();
+         $this->result_from_model = $this->my_report_problem->get_problem_actives();
         } else if($periode1 and $periode2) {
             $this->result_from_model = $this->my_report_problem->get_problem_between_periode($periode1, $periode2);
         }else if($item) {
@@ -107,56 +107,143 @@ class My_report_problem
     //     // the 8 will automatically becoming parameter $id
     //     return $this->my_report_problem->deleteGuest($id);
     // }
-    // public function update_supervisor_by_id($id)
-    // {
-    //     // catch the query string request
-    //     $req = Flight::request();
-    //     $supervisor_name = $req->data->supervisor_name;
-    //     $supervisor_phone = $req->data->supervisor_phone;
-    //     $supervisor_warehouse = $req->data->supervisor_warehouse;
-    //     $supervisor_shift = $req->data->supervisor_shift;
-    //     $is_disabled = $req->data->is_disabled;
-    //     // initiate the column and values to update
-    //     $keyValueToUpdate = null;
-    //     // conditional supervisor_name
-    //     if ($supervisor_name) {
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "supervisor_name='$supervisor_name'"
-    //             : "$keyValueToUpdate, supervisor_name='$supervisor_name'";
-    //     }
+    public function update_problem_by_id($id)
+    {
+        // catch the query string request
+        $req = Flight::request();
+        $warehouse_id = $req->data->warehouse_id;
+        $supervisor_id = $req->data->supervisor_id;
+        $head_spv_id = $req->data->head_spv_id;
+        $item_kode = $req->data->item_kode;
+        $tanggal_mulai = $req->data->tanggal_mulai;
+        $shift_mulai = $req->data->shift_mulai;
+        $pic = $req->data->pic;
+        $dl = $req->data->dl;
+        $masalah = $req->data->masalah;
+        $sumber_masalah = $req->data->sumber_masalah;
+        $solusi = $req->data->solusi;
+        $solusi_panjang = $req->data->solusi_panjang;
+        $dl_panjang = $req->data->dl_panjang;
+        $pic_panjang = $req->data->pic_panjang;
+        $tanggal_selesai = $req->data->tanggal_selesai;
+        $shift_selesai = $req->data->shift_selesai;
+        // initiate the column and values to update
+        $keyValueToUpdate = null;
+        // conditional warehouse_id
+        if ($warehouse_id) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "warehouse_id='$warehouse_id'"
+                : "$keyValueToUpdate, warehouse_id='$warehouse_id'";
+        }
 
-    //     // conditional is_disabled
-    //     if (!is_null($is_disabled)) {
-    //         $value = $is_disabled ? 1 : 0;
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "is_disabled='$value'"
-    //             : "$keyValueToUpdate, is_disabled='$value'";
-    //     }
+        // conditional tanggal_mulai
+        if ($tanggal_mulai) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "tanggal_mulai='$tanggal_mulai'"
+                : "$keyValueToUpdate, tanggal_mulai='$tanggal_mulai'";
+        }
 
-    //     // conditional supervisor_shift
-    //     if ($supervisor_shift) {
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "supervisor_shift='$supervisor_shift'"
-    //             : "$keyValueToUpdate, supervisor_shift='$supervisor_shift'";
-    //     }
+        // conditional shift_mulai
+        if ($shift_mulai){
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "shift_mulai='$shift_mulai'"
+                : "$keyValueToUpdate, shift_mulai='$shift_mulai'";
+        }
 
-    //     // conditional supervisor_warehouse
-    //     if ($supervisor_warehouse) {
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "supervisor_warehouse='$supervisor_warehouse'"
-    //             : "$keyValueToUpdate, supervisor_warehouse='$supervisor_warehouse'";
-    //     }
+        // conditional pic
+        if ($pic) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "pic='$pic'"
+                : "$keyValueToUpdate, pic='$pic'";
+        }
 
-    //     // conditional supervisor_phone
-    //     if ($supervisor_phone) {
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "supervisor_phone='$supervisor_phone'"
-    //             : "$keyValueToUpdate, supervisor_phone='$supervisor_phone'";
-    //     }
-    //     // send to myguest model
-    //     $this->result_from_model = $this->my_report_problem->update_supervisor_by_id($keyValueToUpdate, $id);
-    //     return $this->response();
-    // }
+        // conditional masalah
+        if ($masalah) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "masalah='$masalah'"
+                : "$keyValueToUpdate, masalah='$masalah'";
+        }
+
+        // conditional sumber_masalah
+        if ($sumber_masalah) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "sumber_masalah='$sumber_masalah'"
+                : "$keyValueToUpdate, sumber_masalah='$sumber_masalah'";
+        }
+
+        // conditional solusi
+        if ($solusi) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "solusi='$solusi'"
+                : "$keyValueToUpdate, solusi='$solusi'";
+        }
+
+        // conditional dl_panjang
+        if ($dl_panjang) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "dl_panjang='$dl_panjang'"
+                : "$keyValueToUpdate, dl_panjang='$dl_panjang'";
+        }
+
+        // conditional pic_panjang
+        if ($pic_panjang) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "pic_panjang='$pic_panjang'"
+                : "$keyValueToUpdate, pic_panjang='$pic_panjang'";
+        }
+
+        // conditional tanggal_selesai
+        if ($tanggal_selesai) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "tanggal_selesai='$tanggal_selesai'"
+                : "$keyValueToUpdate, tanggal_selesai='$tanggal_selesai'";
+        }
+
+        // conditional shift_selesai
+        if ($shift_selesai) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "shift_selesai='$shift_selesai'"
+                : "$keyValueToUpdate, shift_selesai='$shift_selesai'";
+        }
+
+        // conditional dl
+        if ($dl) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "dl='$dl'"
+                : "$keyValueToUpdate, dl='$dl'";
+        }
+
+        // conditional solusi_panjang
+        if ($solusi_panjang) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "solusi_panjang='$solusi_panjang'"
+                : "$keyValueToUpdate, solusi_panjang='$solusi_panjang'";
+        }
+
+        // conditional item_kode
+        if ($item_kode) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "item_kode='$item_kode'"
+                : "$keyValueToUpdate, item_kode='$item_kode'";
+        }
+
+        // conditional head_spv_id
+        if ($head_spv_id) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "head_spv_id='$head_spv_id'"
+                : "$keyValueToUpdate, head_spv_id='$head_spv_id'";
+        }
+
+        // conditional supervisor_id
+        if ($supervisor_id) {
+            $keyValueToUpdate = is_null($keyValueToUpdate)
+                ? "supervisor_id='$supervisor_id'"
+                : "$keyValueToUpdate, supervisor_id='$supervisor_id'";
+        }
+        // send to myguest model
+        $this->result_from_model = $this->my_report_problem->update_problem_by_id($keyValueToUpdate, $id);
+        return $this->response();
+    }
     protected function response()
     {
         if ($this->result_from_model) {
