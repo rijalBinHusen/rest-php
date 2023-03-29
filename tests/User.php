63f50f5a2c6759b98dd59cb73009c3ff115ff180
rijalBinHusen <jeryzal333@gmail.com>
2023-03-29 10:53:07 +0700

    login test

---
+++ tests/User.php
@@ -0,0 +1,67 @@
+<?php
+
+class MyRestServerUserTest extends PHPUnit_Framework_TestCase
+{
+    private $url = "http://localhost/rest-php/";
+    // Test register
+    
+    // test login success
+    public function testLoginEndpoint()
+    {
+        // Define the request body
+        $data = array('email' => 'test@test.com', 'password' => '12345');
+        $data_string = json_encode($data);
+        
+        // Set up the request headers
+        $headers = array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string)
+        );
+        
+        // Send a POST request to the /endpoint URL with the request body
+        $ch = curl_init($this->url . "login");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $convertToAssocArray = json_decode($response, true);
+        // Verify that the response same as expected
+        $this->assertArrayHasKey('success', $convertToAssocArray);
+        $this->assertArrayHasKey('token', $convertToAssocArray);
+    }
+    
+    // test login failed   
+    public function testLoginEndpointFailde()
+    {
+        // Define the request body
+        $data = array('email' => 'test@test.com', 'password' => '1234');
+        $data_string = json_encode($data);
+        
+        // Set up the request headers
+        $headers = array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string)
+        );
+        
+        // Send a POST request to the /endpoint URL with the request body
+        $ch = curl_init($this->url . "login");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $convertToAssocArray = json_decode($response, true);
+        // Verify that the response same as expected
+        $this->assertArrayHasKey('success', $convertToAssocArray);
+        $this->assertArrayHasKey('message', $convertToAssocArray);
+    }
+    // test validatin must failed
+
+    // test validation must success
+
+}
