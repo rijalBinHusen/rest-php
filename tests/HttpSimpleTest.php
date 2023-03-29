6c87daf4ba31f458c40beed4098f35dc83a46aa9
rijalBinHusen <jeryzal333@gmail.com>
2023-03-29 07:57:19 +0700

    unit test post method

--- tests/HttpSimpleTest.php
+++ tests/HttpSimpleTest.php
@@ -2,11 +2,37 @@
 
 class MyRestServerTest extends PHPUnit_Framework_TestCase
 {
+    private $url = "http://localhost/rest-php/";
     public function testGetEndpoint()
     {
         // Send a GET request to the /endpoint URL
-        $response = file_get_contents('http://localhost/rest-php/');
+        $response = file_get_contents($this->url);
         // Verify that the response is "I received a GET request."
         $this->assertEquals('I received a GET request.', $response);
+    }
+
+    public function testPostEndpoint()
+    {
+        // Define the request body
+        $data = array('foo' => 'bar');
+        $data_string = json_encode($data);
+        
+        // Set up the request headers
+        $headers = array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string)
+        );
+        
+        // Send a POST request to the /endpoint URL with the request body
+        $ch = curl_init($this->url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        // Verify that the response is "Success"
+        $this->assertEquals('I received a POST request ', $response);
     }
 }
