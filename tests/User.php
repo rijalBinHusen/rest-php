ab5574695decde6800880caf1c9df2ba3b307b8d
rijalBinHusen <jeryzal333@gmail.com>
2023-04-19 14:25:57 +0700

    http call class

--- tests/User.php
+++ tests/User.php
@@ -1,29 +1,18 @@
 <?php
 
+require_once("./httpCall.php");
+
 class MyRestServerUserTest extends PHPUnit_Framework_TestCase
 {
     private $url = "http://localhost/rest-php/";
     // Test register must failed
     public function testRegisterEndpointFailed()
     {
+        $http = new HttpCall($this->url . "register");
         // Define the request body
         $data = array('email' => 'test@test.com', 'password' => "1233333", 'username' => "name0123");
-        $data_string = json_encode($data);
-        
-        // Set up the request headers
-        $headers = array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string)
-        );
-        
-        // Send a POST request to the /endpoint URL with the request body
-        $ch = curl_init($this->url . "register");
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $response = curl_exec($ch);
-        curl_close($ch);
+        $http->setData($data);
+        $response = $http->getResponse("POST");
 
         $convertToAssocArray = json_decode($response, true);
         // Verify that the response same as expected
@@ -36,25 +25,12 @@ class MyRestServerUserTest extends PHPUnit_Framework_TestCase
     // test login success
     public function testLoginEndpoint()
     {
+        $http = new HttpCall($this->url . "login");
         // Define the request body
         $data = array('email' => 'test@test.com', 'password' => '12345');
-        $data_string = json_encode($data);
+        $http->setData($data);
+        $response = $http->getResponse("POST");
         
-        // Set up the request headers
-        $headers = array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string)
-        );
-        
-        // Send a POST request to the /endpoint URL with the request body
-        $ch = curl_init($this->url . "login");
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $response = curl_exec($ch);
-        curl_close($ch);
-
         $convertToAssocArray = json_decode($response, true);
         // Verify that the response same as expected
         $this->assertArrayHasKey('success', $convertToAssocArray);
@@ -69,24 +45,11 @@ class MyRestServerUserTest extends PHPUnit_Framework_TestCase
     // test login failed   
     public function testLoginEndpointFailed()
     {
+        $http = new HttpCall($this->url . "login");
         // Define the request body
         $data = array('email' => 'test@test.com', 'password' => '1234');
-        $data_string = json_encode($data);
-        
-        // Set up the request headers
-        $headers = array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string)
-        );
-        
-        // Send a POST request to the /endpoint URL with the request body
-        $ch = curl_init($this->url . "login");
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $response = curl_exec($ch);
-        curl_close($ch);
+        $http->setData($data);
+        $response = $http->getResponse("POST");
 
         $convertToAssocArray = json_decode($response, true);
         // Verify that the response same as expected
---
+++ tests/httpCall.php
@@ -0,0 +1,40 @@
+<?php
+
+class HttpCall {
+    private $url;
+    private $data_string;
+    private $headers;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function getEndPoint() {
+        return file_get_contents($this->url);
+    }
+
+    public function setData($arrayData) {
+        $this->data_string = json_encode($arrayData);
+        $this->setHeaders();
+    }
+
+    private function setHeaders() {
+        $this->headers = array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($this->data_string)
+        );
+    }
+    
+    public function getResponse($operation) {
+        $ch = curl_init($this->url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $operation);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return $response;
+    }
+}
