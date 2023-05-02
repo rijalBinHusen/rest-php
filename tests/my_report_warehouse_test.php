d1bdb56e43884e22638afa9a62743ccf4ebbced8
rijalBinHusen <jeryzal333@gmail.com>
2023-05-02 13:54:09 +0700

    unit test warehouse get

---
+++ tests/my_report_warehouse_test.php
@@ -0,0 +1,78 @@
+<?php
+
+class MyReportWarehousesTest extends PHPUnit_Framework_TestCase
+{
+    private $url = "http://localhost/rest-php/myreport";
+    public function testGetEndpoint()
+    {
+        // Send a GET request to the /endpoint URL
+        $get = file_get_contents($this->url . '/warehouses');
+        $response = json_decode($get);
+        $responseType = gettype($response);
+        // Verify that the response is "I received a GET request."
+        $this->assertEquals('array', $responseType);
+    }
+
+    // public function testPostEndpoint()
+    // {
+    //     // Define the request body
+    //     $data = array('foo' => 'bar');
+    //     $data_string = json_encode($data);
+        
+    //     // Set up the request headers
+    //     $headers = array(
+    //         'Content-Type: application/json',
+    //         'Content-Length: ' . strlen($data_string)
+    //     );
+        
+    //     // Send a POST request to the /endpoint URL with the request body
+    //     $ch = curl_init($this->url);
+    //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //     $response = curl_exec($ch);
+    //     curl_close($ch);
+        
+    //     // Verify that the response is "Success"
+    //     $this->assertEquals('I received a POST request ', $response);
+    // }
+
+    // public function testPutEndpoint()
+    // {
+    //     // Define the request body
+    //     $data = array('foo' => 'baz');
+    //     $data_string = json_encode($data);
+        
+    //     // Set up the request headers
+    //     $headers = array(
+    //         'Content-Type: application/json',
+    //         'Content-Length: ' . strlen($data_string)
+    //     );
+        
+    //     // Send a PUT request to the /endpoint URL with the request body
+    //     $ch = curl_init($this->url);
+    //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //     $response = curl_exec($ch);
+    //     curl_close($ch);
+        
+    //     // Verify that the response is "Success"
+    //     $this->assertEquals('I received a PUT request.', $response);
+    // }
+
+    // public function testDeleteEndpoint()
+    // {
+    //     // Send a DELETE request to the /endpoint URL
+    //     $ch = curl_init($this->url);
+    //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     $response = curl_exec($ch);
+    //     curl_close($ch);
+        
+    //     // Verify that the response is "Success"
+    //     $this->assertEquals('I received a DELETE request.', $response);
+    // }
+}
