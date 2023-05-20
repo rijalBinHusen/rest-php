8bdf7afda5ec0f52596fcefec9cbc7695a4d60ef
rijalBinHusen <jeryzal333@gmail.com>
2023-05-20 11:49:32 +0700

    base stock unit test in progress

---
+++ tests/my_report_base_stock_test.php
@@ -0,0 +1,332 @@
+<?php
+
+require_once(__DIR__ . '/httpCall.php');
+require_once(__DIR__ . '/../vendor/fakerphp/faker/src/autoload.php');
+
+class MyReportComplainImportTest extends PHPUnit_Framework_TestCase
+{
+    private $url = "http://localhost/rest-php/myreport/";
+    private $idInserted = null;
+    private $urlGets;
+    private $urlPost;
+    private $dataToInsert;
+    private $dataToUpdate;
+
+    public function __construct()
+    {
+        $faker = Faker\Factory::create();
+        $this->dataToInsert = array(
+            'parent' => $faker->text(10),
+            'shift' => $faker->numberBetween(1, 3),
+            'item' => $faker->text(15),
+            'awal' => $faker->numberBetween(1, 10000),
+            'in_stock' => $faker->numberBetween(1, 10000),
+            'out_stock' => $faker->numberBetween(1, 1000000),
+            'date_in' => $faker->date('now'),
+            'plan_out' => $faker->number,
+            'date_out' => false,
+            'date_end' => false,
+            'real_stock' => false,
+            'problem' => false,
+        );
+
+        $this->urlPost = $this->url . 'base_file/';
+        $this->urlGets = $this->url . "base_files?periode1=$periode&periode2=$periode";
+
+        $this->dataToUpdate = array(
+            'awal' => $faker->text(10),
+            'in_stock' => $faker->text(15),
+            'out_stock' => $faker->text(17),
+            'date_in' => true
+        );
+
+    }
+    
+    // public function testPostEndpoint()
+    // {
+    //     $http = new HttpCall($this->urlPost);
+    //     // Define the request body
+
+    //     $http->setData($this->dataToInsert);
+    //     // Define the request body
+    //     $http->addJWTToken();
+    //     $response = $http->getResponse("POST");
+
+    //     $convertToAssocArray = json_decode($response, true);
+
+    //     // fwrite(STDERR, print_r($response, true));
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('data', $convertToAssocArray);
+    //     $this->assertArrayHasKey('id', $convertToAssocArray->data);
+    //     $this->assertEquals(true, $convertToAssocArray['success']);
+    //     $this->idInserted = $convertToAssocArray->data->id;
+    // }
+
+    // public function testPostEndpointFailed()
+    // {
+    //     $faker = Faker\Factory::create();
+    //     $http = new HttpCall($this->urlPost);
+    //     // Define the request body
+    //     $data = array(
+    //         'supervisor_name' => $faker->name('female'),
+    //         'supervisor_phone' => $faker->$faker->numberBetween(100000, 1000000),
+    //         'supervisor_warehouse' => $faker->name('female'),
+    //         'supervisor_shift' => 1,
+    //     );
+
+    //     $http->setData($data);
+
+    //     $http->addJWTToken();
+        
+    //     $response = $http->getResponse("POST");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals('Failed to add base file, check the data you sent', $convertToAssocArray['message']);
+    // }
+
+    // public function testPostEndpointFailed2()
+    // {
+    //     $httpCallVar = new HttpCall($this->urlPost);
+    //     // Define the request body
+
+    //     $httpCallVar->setData($this->dataToInsert);
+        
+    //     $response = $httpCallVar->getResponse("POST");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals('You must be authenticated to access this resource.', $convertToAssocArray['message']);
+    // }
+
+    // public function testGetEndpoint()
+    // {
+    //     $http = new HttpCall($this->urlGets);
+    //     $http->addJWTToken();
+    //     // Send a GET request to the /endpoint URL
+    //     $response = $http->getResponse("GET");
+        
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // fwrite(STDERR, print_r($convertToAssocArray, true));
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('data', $convertToAssocArray);
+    //     $this->assertArrayHasKey('id', $convertToAssocArray->data[0]);
+    //     $this->assertArrayHasKey('periode', $convertToAssocArray->data[0]);
+    //     $this->assertArrayHasKey('warehouse_id', $convertToAssocArray->data[0]);
+    //     $this->assertArrayHasKey('file_name', $convertToAssocArray->data[0]);
+    //     $this->assertArrayHasKey('stock_sheet', $convertToAssocArray->data[0]);
+    //     $this->assertArrayHasKey('clock_sheet', $convertToAssocArray->data[0]);
+    //     $this->assertArrayHasKey('is_imported', $convertToAssocArray->data[0]);
+    //     $this->assertEquals(true, $convertToAssocArray['success']);
+    // }
+
+    // public function testGetEndpointFailed()
+    // {
+    //     $http = new HttpCall($this->urlGets);
+    //     $response = $http->getResponse("GET");
+        
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // fwrite(STDERR, print_r($convertToAssocArray, true));
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("You must be authenticated to access this resource.", $convertToAssocArray['message']);
+    // }
+
+    // public function testGetByIdEndpoint()
+    // {
+    //     $http = new HttpCall($this->urlPost . $this->idInserted);
+    //     $http->addJWTToken();
+    //     // Send a GET request to the /endpoint URL
+    //     $response = $http->getResponse("GET");
+        
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // fwrite(STDERR, print_r($convertToAssocArray, true));
+    //     // Verify that the response same as expected
+        
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('data', $convertToAssocArray);
+    //     $this->assertArrayHasKey('id', $convertToAssocArray->data);
+    //     $this->assertArrayHasKey('periode', $convertToAssocArray->data);
+    //     $this->assertArrayHasKey('warehouse_id', $convertToAssocArray->data);
+    //     $this->assertArrayHasKey('file_name', $convertToAssocArray->data);
+    //     $this->assertArrayHasKey('stock_sheet', $convertToAssocArray->data);
+    //     $this->assertArrayHasKey('clock_sheet', $convertToAssocArray->data);
+    //     $this->assertArrayHasKey('is_imported', $convertToAssocArray->data);
+    //     $this->assertEquals(true, $convertToAssocArray['success']);
+    // }
+
+    // public function testGetByIdEndpointFailed()
+    // {
+    //     $http = new HttpCall($this->urlPost . $this->idInserted);
+        
+    //     // Send a GET request to the /endpoint URL
+    //     $response = $http->getResponse("GET");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // fwrite(STDERR, print_r($convertToAssocArray, true));
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("You must be authenticated to access this resource.", $convertToAssocArray['message']);
+    // }
+
+    // public function testGetByIdEndpointFailed2()
+    // {
+    //     $http = new HttpCall($this->urlPost . $this->idInserted . "11123");
+
+    //     $http->addJWTToken();
+    //     // Send a GET request to the /endpoint URL
+    //     $response = $http->getResponse("GET");
+        
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // fwrite(STDERR, print_r($convertToAssocArray, true));
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("Base file not found", $convertToAssocArray['message']);
+    // }
+
+    // public function testPutEndpoint()
+    // {
+    //     $faker = Faker\Factory::create();
+    //     $httpCallVar = new HttpCall($this->urlPost . $this->idInserted);
+    //     // Define the request body
+
+    //     $httpCallVar->setData($this->dataToUpdate);
+    //     $httpCallVar->addJWTToken();
+        
+    //     $response = $httpCallVar->getResponse("PUT");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(true, $convertToAssocArray['success']);
+    //     $this->assertEquals("Update base file success", $convertToAssocArray['message']);
+    // }
+
+    // public function testPutEndpointFailed()
+    // {
+    //     // error 400
+    //     $http = new HttpCall($this->urlPost . $this->idInserted);
+    //     // Define the request body
+    //     $data = array(
+    //         'item_kode33' => '330303030',
+    //         'item_name33' => 'false false false',
+    //         'last_used33' => 'false false false'
+    //     );
+
+    //     $http->setData($data);
+
+    //     $http->addJWTToken();
+        
+    //     $response = $http->getResponse("PUT");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals('Failed update base file, check the data you sent', $convertToAssocArray['message']);
+    // }
+
+    // public function testPutEndpointFailed2()
+    // {
+    //     // error 401
+    //     $httpCallVar = new HttpCall($this->urlPost . $this->idInserted);
+    //     // Define the request body
+
+    //     $httpCallVar->setData($this->dataToUpdate);
+        
+    //     $response = $httpCallVar->getResponse("PUT");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("You must be authenticated to access this resource.", $convertToAssocArray['message']);
+    // }
+
+    // public function testPutEndpointFailed3()
+    // {
+    //     // error 404
+    //     $faker = Faker\Factory::create();
+    //     $httpCallVar = new HttpCall($this->urlPost . $this->idInserted . '333');
+    //     // Define the request body
+
+    //     $httpCallVar->setData($this->dataToUpdate);
+
+    //     $httpCallVar->addJWTToken();
+        
+    //     $response = $httpCallVar->getResponse("PUT");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("Field problem not found", $convertToAssocArray['message']);
+    // }
+
+    // public function testDeleteEndpoint()
+    // {
+    //     $httpCallVar = new HttpCall($this->urlPost . $this->idInserted);
+
+    //     $httpCallVar->addJWTToken();
+        
+    //     $response = $httpCallVar->getResponse("DELETE");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(true, $convertToAssocArray['success']);
+    //     $this->assertEquals("Delete base file success", $convertToAssocArray['message']);
+    // }
+
+    // public function testDeleteEndpointFailed2()
+    // {
+    //     // error 401
+    //     $httpCallVar = new HttpCall($this->urlPost . $this->idInserted);
+        
+    //     $response = $httpCallVar->getResponse("DELETE");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("You must be authenticated to access this resource.", $convertToAssocArray['message']);
+    // }
+
+    // public function testDeleteEndpointFailed3()
+    // {
+    //     // error 404
+    //     $httpCallVar = new HttpCall($this->urlPost . $this->idInserted);
+
+    //     $httpCallVar->addJWTToken();
+        
+    //     $response = $httpCallVar->getResponse("DELETE");
+
+    //     $convertToAssocArray = json_decode($response, true);
+    //     // Verify that the response same as expected
+    //     $this->assertArrayHasKey('success', $convertToAssocArray);
+    //     $this->assertArrayHasKey('message', $convertToAssocArray);
+    //     $this->assertEquals(false, $convertToAssocArray['success']);
+    //     $this->assertEquals("Base file not found", $convertToAssocArray['message']);
+    // }
+
+}
