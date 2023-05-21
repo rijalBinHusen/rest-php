93d51d843ac7a8aed9ddb63876ecd013898774ad
rijal <jeryzal333@gmail.com>
2023-05-21 19:41:51 +0700

    warehouse end point

--- app/myreport/warehouse/warehouse_controller.php
+++ app/myreport/warehouse/warehouse_controller.php
@@ -48,12 +48,22 @@ class My_report_warehouse
                 // append warehouse
                 $result = $this->my_report_warehouse->append_warehouse($warehouse_name, $warehouse_group, $warehouse_supervisors);
             }
+
+            if($this->my_report_warehouse->is_success !== true) {
+                Flight::json(
+                    array(
+                        'success'=> false,
+                        'message'=> $this->my_report_warehouse->is_success
+                    ), 500
+                );
+                return;
+            }
             
             Flight::json(
                 array(
                     'success' => true,
-                    'data' => $result
-                ), 200
+                    'id' => $result
+                ), 201
             );
             return;
         }
@@ -65,61 +75,113 @@ class My_report_warehouse
             ), 400
         );
     }
-    // public function get_warehouse_by_id($id)
-    // {
-    //     // myguest/8
-    //     // the 8 will automatically becoming parameter $id
-    //     $this->result_from_model = $this->my_report_warehouse->get_warehous_by_id($id);
-    //     return $this->response();
-    // }
+    public function get_warehouse_by_id($id)
+    {
+        // myguest/8
+        // the 8 will automatically becoming parameter $id
+        $result = $this->my_report_warehouse->get_warehous_by_id($id);
+
+        $is_success = $this->my_report_warehouse->is_success;
+
+        $is_found = count($result) > 0;
+
+        if($is_success === true && $is_found) {
+            Flight::json(
+                array(
+                    'success' => true,
+                    'data' => $result
+                )
+            );
+        }
+
+        else if($is_success !== true) {
+            Flight::json(
+                array(
+                    'success' => false,
+                    'message' => $is_success
+                ), 500
+            );
+            return;
+        }
+
+        else {
+            Flight::json(
+                array(
+                    'success' => false,
+                    'message' => 'Warehouse not found'
+                )
+            );
+        }
+    }
+
     // // public function deleteGuest($id) {
     // //     // myguest/8
     // //     // the 8 will automatically becoming parameter $id
     // //     return $this->my_report_warehouse->deleteGuest($id);
     // // }
-    // public function update_warehouse_by_id($id)
-    // {
-    //     // catch the query string request
-    //     $req = Flight::request();
-    //     $warehouse_name = $req->data->warehouse_name;
-    //     $warehouse_group = $req->data->warehouse_group;
-    //     // initiate the column and values to update
-    //     $keyValueToUpdate = null;
-    //     // conditional warehouse_name
-    //     if ($warehouse_name) {
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "warehouse_name='$warehouse_name'"
-    //             : "$keyValueToUpdate, warehouse_name='$warehouse_name'";
-    //     }
 
-    //     // conditional warehouse$warehouse_group
-    //     if ($warehouse_group) {
-    //         $keyValueToUpdate = is_null($keyValueToUpdate)
-    //             ? "warehouse_group='$warehouse_group'"
-    //             : "$keyValueToUpdate, warehouse_group='$warehouse_group'";
-    //     }
-    //     // send to myguest model
-    //     $this->result_from_model = $this->my_report_warehouse->update_warehouse_by_id($keyValueToUpdate, $id);
-    //     return $this->response();
-    // }
-    // protected function response()
-    // {
-    //     if ($this->result_from_model) {
-    //         // set the http status code 200
-    //         $this->code = 200;
-    //         // set the result data that would be return to user
-    //         $this->result = $this->result_from_model;
-    //     } else {
-    //         // set the http status code 200
-    //         $this->code = 400;
-    //     }
-    //     // return the result
-    //     return Flight::json(
-    //         // the result
-    //         $this->result
-    //         // and the code
-    //         ,
-    //         $this->code
-    //     );
-    // }
+    public function update_warehouse_by_id($id)
+    {
+        // catch the query string request
+        $req = Flight::request();
+        $warehouse_name = $req->data->warehouse_name;
+        $warehouse_group = $req->data->warehouse_group;
+
+        $invalid_request_body = is_null($warehouse_name) || is_null($warehouse_group) || is_null($id);
+
+        if($invalid_request_body) {
+            Flight::json(
+                array(
+                    'success' => false,
+                    'message' => 'Failed to update warehouse, check the data you sent'
+                )
+            );
+            return;
+        }
+
+        // initiate the column and values to update
+        $keyValueToUpdate = array();
+        // conditional warehouse_name
+        if ($warehouse_name) {
+            $keyValueToUpdate["warehouse_name"] = $warehouse_name;
+        }
+
+        // conditional warehouse$warehouse_group
+        if ($warehouse_group) {
+            $keyValueToUpdate["warehouse_group"] = $warehouse_group;
+        }
+
+        $result = $this->my_report_warehouse->update_warehouse_by_id($keyValueToUpdate, "id", $id);
+
+        $is_success = $this->my_report_warehouse->is_success;
+
+        if($is_success === true) {
+            Flight::json(
+                array(
+                    'success' => true,
+                    'message' => 'Update warehouse success'
+                )
+            );
+        }
+
+        else if($is_success !== true) {
+            Flight::json(
+                array(
+                    'success' => false,
+                    'message' => $is_success
+                ), 500
+            );
+            return;
+        }
+
+        else {
+            Flight::json(
+                array(
+                    'success' => false,
+                    'message' => 'Warehouse not found'
+                )
+            );
+        }
+        
+    }
 }
--- app/myreport/warehouse/warehouse_route.php
+++ app/myreport/warehouse/warehouse_route.php
@@ -23,3 +23,25 @@ Flight::route('POST /myreport/warehouse', function () {
     $myreport_warehouse = new My_report_warehouse();
     $myreport_warehouse->add_warehouse();    
 });
+
+Flight::route("GET /warehouse/@id", function ($id) {
+    $is_token_valid = $user->check_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_warehouse = new My_report_warehouse();
+    $myreport_warehouse->get_warehouse_by_id($id);    
+});
+
+Flight::route("GET /warehouse/@id", function ($id) {
+    $is_token_valid = $user->check_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_warehouse = new My_report_warehouse();
+    $myreport_warehouse->updadte_warehouse_by_id($id);
+});
