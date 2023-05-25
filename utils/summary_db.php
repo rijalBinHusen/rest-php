a6a38188e463825e321a9affc07a6f2aa3ec5099
rijalBinHusen <jeryzal333@gmail.com>
2023-05-25 14:27:27 +0700

    set largest last id

--- utils/summary_db.php
+++ utils/summary_db.php
@@ -15,6 +15,7 @@ Class SummaryDatabase {
         $this->database = new Query_builder();
         $this->table = $table;
         $this->table_as_id = str_replace("my_report_", "", $table);
+        $this->getLastId();
     }
 
     public function getLastId() {
@@ -24,29 +25,28 @@ Class SummaryDatabase {
         $this->last_id = $summary['last_id'];
         // nextId
         $lastId = $this->last_id ? $this->last_id : generateId($this->table_as_id ."_22320000");
+        $this->is_table_exists = !empty($summary['last_id']) || !is_null($summary['last_id']);
         
         return $lastId;
     }
 
 
     public function getNextId() {
-        $summary = $this->database->select_where("summary", 'table_name', $this->table)->fetch();
         
-        $this->total = $summary['total'];
-        $this->last_id = $summary['last_id'];
-        $this->is_table_exists = !empty($summary['last_id']) || !is_null($summary['last_id']);
         // nextId
         $nextId = $this->last_id ? generateId($this->last_id) : generateId($this->table_as_id ."_22320000");
+
         $this->last_id = $nextId;
         
         return $nextId;
     }
 
     public function updateLastId($your_last_id) {
-        $is_empty_last_id = is_null($your_last_id) || empty($your_last_id);
 
-        $what_last_id_to_set = $is_empty_last_id ? $this->last_id : $your_last_id;
-        
+        $all_last_id = array($your_last_id, $this->last_id);
+
+        $what_last_id_to_set = max($all_last_id);
+                
         $data = array(
             'table_name' => $this->table,
             'last_id' => $what_last_id_to_set,
