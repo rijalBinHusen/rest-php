446d2c699d21c2767207d39afbddfa6b23367317
rijalBinHusen <jeryzal333@gmail.com>
2023-05-26 16:25:03 +0700

    document model

---
+++ app/myreport/document/document_model.php
@@ -0,0 +1,236 @@
+<?php
+require_once(__DIR__ . '/../../../utils/database.php');
+require_once(__DIR__ . '/../../../utils/summary_db.php');
+
+class My_report_document_model
+{
+    protected $database;
+    var $table = "my_report_report_document";
+    var $is_success = true;
+    private $summary = null;
+
+    function __construct()
+    {
+        $this->database = new Query_builder();
+        $this->summary = new SummaryDatabase($this->table);
+    }
+
+    public function append_document(
+        $collected,
+        $approval,
+        $status,
+        $shared,
+        $finished,
+        $total_do,
+        $total_kendaraan,
+        $total_waktu,
+        $base_report_file,
+        $is_finished,
+        $supervisor_id,
+        $periode,
+        $shift,
+        $head_spv_id,
+        $warehouse_id,
+        $is_generated_document
+    )
+    {
+        $nextId = $this->summary->getNextId();
+        // write to database
+        $this->write_document(
+            $nextId,
+            $collected,
+            $approval,
+            $status,
+            $shared,
+            $finished,
+            $total_do,
+            $total_kendaraan,
+            $total_waktu,
+            $base_report_file,
+            $is_finished,
+            $supervisor_id,
+            $periode,
+            $shift,
+            $head_spv_id,
+            $warehouse_id,
+            $is_generated_document
+        );
+
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+
+        } else {
+
+            return $nextId;
+
+        }
+
+    }
+
+    public function write_document(
+        $id,
+        $collected,
+        $approval,
+        $status,
+        $shared,
+        $finished,
+        $total_do,
+        $total_kendaraan,
+        $total_waktu,
+        $base_report_file,
+        $is_finished,
+        $supervisor_id,
+        $periode,
+        $shift,
+        $head_spv_id,
+        $warehouse_id,
+        $is_generated_document
+    )
+    {
+
+        $data_to_insert = array(
+            "id" => $id,
+            'collected' => $collected,
+            'approval' => $approval,
+            'status' =>$status,
+            'shared' => $shared,
+            'finished' => $finished,
+            'total_do' => $total_do,
+            'total_kendaraan' => $total_kendaraan,
+            'total_waktu' => $total_waktu,
+            'base_report_file' => $base_report_file,
+            'is_finished' => $is_finished,
+            'supervisor_id' => $supervisor_id,
+            'periode' => $periode,
+            'shift' => $shift,
+            'head_spv_id' => $head_spv_id,
+            'warehouse_id' => $warehouse_id,
+            'is_generated_document' => $is_generated_document
+        );
+
+        $this->database->insert($this->table, $data_to_insert);
+
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+
+        } else {
+
+            $this->summary->updateLastId($id);
+            return $id;
+
+        }
+
+    }
+
+    public function get_documents_by_periode($periode1, $periode2)
+    {
+        $query = "SELECT * FROM $this->table WHERE periode BETWEEN $periode1 AND $periode2";
+        $result  = $this->database->sqlQuery($query)->fetchAll(PDO::FETCH_ASSOC);
+        
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+            
+        }
+        else {
+
+            return $result;
+
+        }
+    }
+
+    public function get_documents_by_status($status)
+    {
+        $result  = $this->database->select_where($this->table, 'is_finished', $status)->fetchAll(PDO::FETCH_ASSOC);
+        
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+            
+        }
+
+        else {
+
+            return $result;
+
+        }
+    }
+    
+    public function get_document_by_id($id)
+    {
+
+        $result = $this->database->select_where($this->table, 'id', $id)->fetchAll(PDO::FETCH_ASSOC);
+        
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+            return array();
+
+        } else {
+
+            return $result;
+        }
+
+    }
+
+    public function update_document_by_id(array $data, $id)
+    {
+
+        $result = $this->database->update($this->table, $data, 'id', $id);
+
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+
+        } else {
+
+            return $result;
+
+        }
+
+    }
+
+    public function remove_document_by_id($id)
+    {
+
+        $result = $this->database->delete($this->table, 'id', $id);
+
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+
+        } else {
+
+            return $result;
+
+        }
+
+    }
+
+    public function last_document_date()
+    {
+        $query = "SELECT * FROM $this->table ORDER BY periode DESC LIMIT 1";
+
+        $result = $this->database->sqlQuery($query)->fetchAll(PDO::FETCH_ASSOC);
+
+        $not_exists = count($result) < 1;
+
+        if($this->database->is_error !== null) {
+
+            $this->is_success = $this->database->is_error;
+
+        } else if($not_exists) {
+
+            $this->is_success = $this->database->is_error;
+            return time();
+
+        } else {
+
+            return $result['periode'];
+
+        }
+
+    }
+}
---
+++ app/myreport/document/document_route.php
@@ -0,0 +1,86 @@
+<?php
+require_once(__DIR__ . "/../../Users/user_controller.php");
+require_once(__DIR__ . "/problem_controller.php");
+
+Flight::route('POST /myreport/problem', function () {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->add_problem();    
+});
+
+Flight::route('GET /myreport/problems/byperiode', function () {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->get_problem_by_periode();
+});
+
+Flight::route('GET /myreport/problems/bystatus', function () {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->get_problem_by_status();
+});
+
+Flight::route('GET /myreport/problems/bysupervisor', function () {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->get_problem_by_supervisor();
+});
+
+Flight::route('GET /myreport/problems/bywarehouseanditem', function () {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->get_problem_by_warehouse_and_item();
+});
+
+Flight::route("GET /myreport/problem/@id", function ($id) {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->get_problem_by_id($id);    
+});
+
+Flight::route("PUT /myreport/problem/@id", function ($id) {
+    $user = new User();
+    $is_token_valid = $user->is_valid_token();
+
+    if(!$is_token_valid) {
+        return;
+    }
+
+    $myreport_problem = new My_report_problem();
+    $myreport_problem->update_problem_by_id($id);
+});
