983cb569c3e93c45aa8c128bd24229f95b24225a
rijalBinHusen <jeryzal333@gmail.com>
2023-06-05 16:11:17 +0700

    we're still failed

--- unit_test_my_report.sh
+++ unit_test_my_report.sh
@@ -32,3 +32,5 @@ phpunit tests/my_report/my_report_document_test.php
 sleep 6
 
 #run all those command with this ./unit_test_my_report.sh
+
+# multiple test unit_test_my_report.sh && unit_test_my_report.sh && unit_test_my_report.sh && unit_test_my_report.sh && unit_test_my_report.sh
