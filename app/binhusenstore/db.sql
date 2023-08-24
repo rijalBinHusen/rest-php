27e39fa37f0b731176d2dc8075db51ada5136709
rijalBinHusen <jeryzal333@gmail.com>
2023-08-24 09:47:41 +0700

    command to create order table

--- app/binhusenstore/db.sql
+++ app/binhusenstore/db.sql
@@ -85,3 +85,36 @@ CREATE EVENT truncate_binhusenstore_categories_prefix_seq_event
 ON SCHEDULE EVERY 1 WEEK
 DO
     TRUNCATE TABLE binhusenstore_categories_prefix;
+
+-- ============================== BORDER ======================
+CREATE TABLE if not exists binhusenstore_orders (
+    id VARCHAR(9) NOT NULL PRIMARY KEY, 
+    date_order VARCHAR(10),
+    id_group VARCHAR(9),
+    is_group TINYINT(1),
+    id_product VARCHAR(9),
+    name_of_customer VARCHAR(47),
+    sent VARCHAR(10),
+    title VARCHAR(47),
+    total_balance INT
+);
+
+-- THE PREFIX FOR CUSTOM ID binhusenstore_orders
+CREATE TABLE if not exists binhusenstore_orders_prefix(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+-- CUSTOM UNIQUEEE ID BASED ON PREFIX
+DELIMITER $$
+    CREATE TRIGGER tg_binhusenstore_orders_insert
+    BEFORE INSERT ON binhusenstore_orders
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO binhusenstore_orders_prefix VALUES (NULL, WEEK(CURRENT_DATE));
+        SET NEW.id = CONCAT('O', RIGHT(YEAR(CURRENT_DATE), 2), LPAD(WEEK(CURRENT_DATE), 2, '0'), LPAD(LAST_INSERT_ID(), 4, '0'));
+    END$$
+DELIMITER ;
+
+-- RESET PREFIX TO 0 EVERY WEEK
+CREATE EVENT truncate_binhusenstore_orders_prefix_seq_event
+ON SCHEDULE EVERY 1 WEEK
+DO
+    TRUNCATE TABLE binhusenstore_orders_prefix;
