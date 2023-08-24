23c82273b1f1d2a120669f6453dac402b3edc118
rijalBinHusen <jeryzal333@gmail.com>
2023-08-24 08:49:47 +0700

    add trigger and event to db

--- app/binhusenstore/db.sql
+++ app/binhusenstore/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE if not exists binhusenstore_products (
-    id VARCHAR(30) NOT NULL PRIMARY KEY, 
+    id VARCHAR(9) NOT NULL PRIMARY KEY, 
     name TEXT, 
     categories VARCHAR(96),
     price INT,
@@ -11,9 +11,51 @@ CREATE TABLE if not exists binhusenstore_products (
     FULLTEXT(categories)
 );
 
+-- THE PREFIX FOR CUSTOM ID binhusenstore_products
+CREATE TABLE if not exists binhusenstore_products_prefix(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+-- CUSTOM UNIQUEEE ID BASED ON PREFIX
+DELIMITER $$
+    CREATE TRIGGER tg_binhusenstore_products_insert
+    BEFORE INSERT ON binhusenstore_products
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO binhusenstore_products_prefix VALUES (NULL, WEEK(CURRENT_DATE));
+        SET NEW.id = CONCAT('P', RIGHT(YEAR(CURRENT_DATE), 2), LPAD(WEEK(CURRENT_DATE), 2, '0'), LPAD(LAST_INSERT_ID(), 4, '0'));
+    END$$
+DELIMITER ;
+
+-- RESET PREFIX TO 0 EVERY WEEK
+CREATE EVENT truncate_binhusenstore_products_prefix_seq_event
+ON SCHEDULE EVERY 1 WEEK
+DO
+    TRUNCATE TABLE binhusenstore_products_prefix;
+
+
+-- ============================== BORDER
 CREATE TABLE if not exists binhusenstore_carts (
-    id VARCHAR(30) NOT NULL PRIMARY KEY, 
+    id VARCHAR(9) NOT NULL PRIMARY KEY, 
     id_user VARCHAR(30),
     product_id VARCHAR(30),
     qty TINYINT(2)
 );
+
+-- THE PREFIX FOR CUSTOM ID binhusenstore_carts
+CREATE TABLE if not exists binhusenstore_carts_prefix(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+-- CUSTOM UNIQUEEE ID BASED ON PREFIX
+DELIMITER $$
+    CREATE TRIGGER tg_binhusenstore_carts_insert
+    BEFORE INSERT ON binhusenstore_carts
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO binhusenstore_carts_prefix VALUES (NULL, WEEK(CURRENT_DATE));
+        SET NEW.id = CONCAT('P', RIGHT(YEAR(CURRENT_DATE), 2), LPAD(WEEK(CURRENT_DATE), 2, '0'), LPAD(LAST_INSERT_ID(), 4, '0'));
+    END$$
+DELIMITER ;
+
+-- RESET PREFIX TO 0 EVERY WEEK
+CREATE EVENT truncate_binhusenstore_carts_prefix_seq_event
+ON SCHEDULE EVERY 1 WEEK
+DO
+    TRUNCATE TABLE binhusenstore_carts_prefix;
