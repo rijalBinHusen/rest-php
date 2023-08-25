aea3425c1f53ba75e6a3f2f7156f878bf995a894
rijal <jeryzal333@gmail.com>
2023-08-25 13:52:40 +0700

    database note app command

---
+++ app/notes/db.sql
@@ -0,0 +1,32 @@
+CREATE TABLE
+    if not exists note_app (
+        id VARCHAR(9) NOT NULL PRIMARY KEY,
+        tanggal DATETIME DEFAULT CURRENT_TIMESTAMP,
+        isi TEXT,
+        FULLTEXT(isi),
+        owner VARCHAR(9)
+    );
+
+-- THE PREFIX FOR CUSTOM ID note_app
+
+CREATE TABLE
+    if not exists note_app_prefix(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+-- CUSTOM UNIQUEEE ID BASED ON PREFIX
+
+DELIMITER $$
+
+CREATE TRIGGER tg_note_app_insert 
+BEFORE INSERT ON note_app 
+FOR EACH ROW 
+BEGIN 
+	INSERT INTO note_app_prefix VALUES (NULL, WEEK(CURRENT_DATE));
+	SET NEW.id = CONCAT('N', RIGHT(YEAR(CURRENT_DATE), 2), LPAD(WEEK(CURRENT_DATE), 2, '0'), LPAD(LAST_INSERT_ID(), 4, '0'));
+	END$$ 
+DELIMITER ;
+
+-- RESET PREFIX TO 0 EVERY WEEK
+
+CREATE EVENT truncate_note_app_prefix_seq_event 
+ON SCHEDULE EVERY 1 WEEK
+	DO TRUNCATE TABLE note_app_prefix;
