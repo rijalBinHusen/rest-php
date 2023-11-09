35214867ee0b2039c2e52807f246835a866085f3
rijalbinhusen <jeryzal333@gmail.com>
2023-11-09 13:15:20 +0700

    passing supervisor unit test

--- tests/my_report/my_report_supervisor_test.php
+++ tests/my_report/my_report_supervisor_test.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once(__DIR__ . '/../httpCall.php');
-require_once(__DIR__ . '/../../vendor/fakerphp/faker/src/autoload.php');
+require_once(__DIR__ . '/../../vendor/autoload.php');
 
-class MyReportSupervisorsTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class My_report_supervisor_test extends TestCase
 {
     private $url = "myreport/";
     private $url_host_id = null;
