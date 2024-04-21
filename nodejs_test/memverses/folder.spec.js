c18771b719145e149208080528b33dda2dfccd17
rijal <jeryzal333@gmail.com>
2024-04-21 09:32:28 +0700

    memverses folder and user test

---
+++ nodejs_test/memverses/folder.spec.js
@@ -0,0 +1,163 @@
+import { expect } from "chai";
+import { describe, expect, it } from "vitest"
+import { FetchRequest } from "./fetch_request";
+import { faker } from "@faker-js/faker"
+
+describe("Memverses Folder", async () => {
+
+
+    const fetchReq = new FetchRequest();
+    await fetchReq.loginAdmin("mem_test@test.com", "123456", "memverses/user/login");
+    const newFolder = {
+        id_folder: faker.number.int({ max: 100 }),
+        name: faker.string.sample({ max: 12 }),
+        total_verse_to_show: faker.number.int({ max: 50 }),
+        show_next_chapter_on_second: faker.number.int({ max: 10 }),
+        read_target: faker.number.int({ max: 70 }),
+        is_show_first_letter: false,
+        is_show_tafseer: true,
+        arabic_size: faker.number.int({ max: 40 })
+    }
+
+    let idFolderCreated = "";
+
+    it("Should be create new folder", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folder", newFolder, "POST", true)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(201);
+        expect(responseJSON.success).equal(true);
+        idFolderCreated = responseJSON.id;
+    })
+
+    it("Failed create folder invalid request body", async () => {
+
+        const body = {
+            id_folder: faker.number.int({ max: 100 }),
+            name: faker.string.sample({ max: 12 }),
+            total_verse_to_show: faker.number.int({ max: 50 }),
+            show_next_chapter_on_second: faker.number.int({ max: 10 }),
+            read_target: faker.number.int({ max: 70 }),
+            is_show_first_letter: false,
+            arabic_size: faker.number.int({ max: 40 })
+        }
+
+        const response = await fetchReq.doFetch("memverses/folder", body, "POST", true)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(400);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Failed to add folder, check the data you sent");
+
+    })
+
+    it("Failed create new folder because non authenticated", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folder", newFolder, "POST", false)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(401);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("You must be authenticated to access this resource.");
+
+    })
+
+    it("Should get folders", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folders", false, "GET", true)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.data.length).not.equal(0);
+
+    })
+
+    it("Failed get folders because non authenticated", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folders", false, "GET")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(401);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("You must be authenticated to access this resource.");
+    })
+
+    it("Should get folder by id", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folder?id=" + idFolderCreated, false, "GET", true)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.data.name).equal(newFolder.name);
+    })
+
+    it("Failed get folder because not authenticated", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folder?id=" + idFolderCreated, false, "GET")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(401);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("You must be authenticated to access this resource.");
+    })
+
+    it("Folder not found", async () => {
+
+        const response = await fetchReq.doFetch("memverses/folder?id=ldldl", false, "GET")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(404);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Folder not found");
+    })
+
+    it("Update admin charge should be success", async () => {
+
+        const body = { name: "Bismillah" }
+
+        const response = await fetchReq.doFetch("memverses/folder?id=folder" + idFolderCreated, body, "PUT", true)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.message).equal("Update folder success");
+
+        const responseGET = await fetchReq.doFetch("memverses/folder?id=" + idFolderCreated, false, "GET", true)
+        const getResponseJSON = await responseGET.json();
+
+        expect(responseGET.status).equal(200);
+        expect(getResponseJSON.success).equal(true);
+        expect(getResponseJSON.data.name).equal(body.name);
+
+    })
+
+    it("Failed update because body invalid", async () => {
+
+        const body = { 'failed': "failed test" }
+
+        const response = await fetchReq.doFetch("memverses/folder?id=" + idFolderCreated, body, "PUT", true)
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(400);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Failed to update folder, check the data you sent");
+
+    })
+
+    it("Failed update unkown folder", async () => {
+
+        const body = { 'name': 60000 }
+
+        const response = await fetchReq.doFetch("memverses/folder?id=folder" + "ksdjfh", body, "PUT")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(404);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Folder not found");
+
+    })
+
+})
---
+++ nodejs_test/memverses/user.spec.js
@@ -0,0 +1,100 @@
+import { expect } from "chai";
+import { describe, expect, it } from "vitest"
+import { FetchRequest } from "./fetch_request";
+import { faker } from "@faker-js/faker"
+
+describe("Memverses user end point test", () => {
+
+    const fetchReq = new FetchRequest();
+    const newAccount = { email: faker.string.sample({ max: 13 }) + "@test.com", password: faker.number.int({ max: 13 }), name: faker.person.firstName() };
+
+    it("New account should be registered", async () => {
+
+        const response = await fetchReq.doFetch("memverses/user/register", newAccount, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.message).equal("Registration success.");
+    })
+
+    it("Register failed because user exists", async () => {
+
+        const response = await fetchReq.doFetch("memverses/user/register", newAccount, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(409);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("User exist.");
+    })
+
+    it("Register failed because not contain name", async () => {
+
+        const body = { email: faker.string.sample({ max: 10 }) + "@test.com", password: '123123' };
+        const response = await fetchReq.doFetch("memverses/user/register", body, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(422);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Unprocessable Entity");
+    })
+
+    it("Login new account should be success", async () => {
+
+        const response = await fetchReq.doFetch("memverses/user/login", newAccount, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.token).not.equal("");
+    })
+
+    it("Login with wrong password", async () => {
+        const body = { email: newAccount.email, password: 'sdflkj123' };
+        const response = await fetchReq.doFetch("memverses/user/login", body, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(401);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Invalid memverses/user/password");
+    })
+
+    it("Validate token", async () => {
+        const response = await fetchReq.doFetch("memverses/user/validate", false, "POST", true);
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.message).equal("Valid token");
+    })
+
+    it("Update password failed, because wrong old password", async () => {
+        const body = { password_old: 'sdflkj123', password_new: "sdlkkfjsldkfj" };
+        const response = await fetchReq.doFetch("memverses/user/login", body, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(401);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Wrong password");
+    })
+
+    it("Update password failed, because no new password", async () => {
+        const body = { password_old: newAccount.password, password_new: "" };
+        const response = await fetchReq.doFetch("memverses/user/login", body, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(401);
+        expect(responseJSON.success).equal(false);
+        expect(responseJSON.message).equal("Wrong password");
+    })
+
+    it("Update password success", async () => {
+        const body = { password_old: newAccount.password, password_new: "123456" };
+        const response = await fetchReq.doFetch("memverses/user/login", body, "POST")
+        const responseJSON = await response.json();
+
+        expect(response.status).equal(200);
+        expect(responseJSON.success).equal(true);
+        expect(responseJSON.message).equal("Update password success.");
+    })
+})
