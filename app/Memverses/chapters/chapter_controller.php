eacd83082f4965d4c70967e49997536b9c8e11f1
rijalBinHusen <jeryzal333@gmail.com>
2024-04-26 10:22:22 +0700

    memverses chapter end point

---
+++ app/Memverses/chapters/chapter_controller.php
@@ -0,0 +1,231 @@
+<?php
+require_once(__DIR__ . '/chapter_model.php');
+
+class Memverses_chapter
+{
+    protected $memverses_chapter;
+    function __construct()
+    {
+        $this->memverses_chapter = new Memverses_chapter_model();
+    }
+
+    public function add_chapter($id_user)
+    {
+        // request
+        $req = Flight::request();
+        $id_chapter = $req->data->id_chapter;
+        $id_chapter_client = $req->data->id_chapter_client;
+        $chapter = $req->data->chapter;
+        $verse = $req->data->verse;
+        $readed_times = $req->data->readed_times;
+
+        $valid_request_body = !is_null($id_chapter)
+            && is_string($id_chapter)
+            && !is_null($id_chapter_client)
+            && is_string($id_chapter_client)
+            && !is_null($chapter)
+            && is_numeric($chapter)
+            && !is_null($verse)
+            && is_numeric($verse)
+            && !is_null($readed_times)
+            && is_numeric($readed_times);
+
+        $result = null;
+
+        if ($valid_request_body) {
+
+            $result = $this->memverses_chapter->append_chapter($id_chapter, $id_user, $chapter, $verse, 0);
+
+            if ($this->memverses_chapter->is_success !== true) {
+
+                Flight::json(
+                    array(
+                        'success' => false,
+                        'message' => $this->memverses_chapter->is_success
+                    ),
+                    500
+                );
+            } else {
+
+                Flight::json(
+                    array(
+                        'success' => true,
+                        'id' => $result
+                    ),
+                    201
+                );
+            }
+        }
+
+        Flight::json(
+            array(
+                'success' => false,
+                'message' => 'Failed to add chapter, check the data you sent'
+            ),
+            400
+        );
+    }
+    public function get_chapters($id_user, $id_folder)
+    {
+
+        $result = $this->memverses_chapter->get_chapters($id_user, $id_folder);
+
+        $is_found = count($result) > 0;
+        $is_success = $this->memverses_chapter->is_success;
+
+        if ($is_success === true && $is_found) {
+            Flight::json(
+                array(
+                    "success" => true,
+                    "data" => $result
+                ),
+                200
+            );
+        } else if ($is_success !== true) {
+            Flight::json(
+                array(
+                    "success" => false,
+                    "message" => $result
+                ),
+                500
+            );
+        } else {
+            Flight::json(
+                array(
+                    "success" => false,
+                    "message" => "chapter not found"
+                ),
+                404
+            );
+        }
+    }
+    // public function get_chapter_by_id($id_user, $id_chapter)
+    // {
+
+    //     $result = $this->memverses_chapter->get_chapter_by_id($id_user, $id_chapter);
+
+    //     $is_success = $this->memverses_chapter->is_success;
+    //     $is_found = count($result) > 0;
+
+    //     if ($is_success === true && $is_found) {
+    //         Flight::json(
+    //             array(
+    //                 'success' => true,
+    //                 'data' => $result
+    //             )
+    //         );
+    //     } else if ($is_success !== true) {
+    //         Flight::json(
+    //             array(
+    //                 'success' => false,
+    //                 'message' => $is_success
+    //             ),
+    //             500
+    //         );
+    //     } else {
+    //         Flight::json(
+    //             array(
+    //                 'success' => false,
+    //                 'message' => 'chapter not found'
+    //             ),
+    //             404
+    //         );
+    //     }
+    // }
+
+    // public function remove_chapter($id) {
+    //     // myguest/8
+    //     // the 8 will automatically becoming parameter $id
+    //     $result = $this->memverses_chapter->remove_chapter($id);
+
+    //     $is_success = $this->memverses_chapter->is_success;
+
+    //     if($is_success === true && $result > 0) {
+    //         Flight::json(
+    //             array(
+    //                 'success' => true,
+    //                 'message' => 'Delete chapter success',
+    //             )
+    //         );
+    //     }
+
+    //     else if($is_success !== true) {
+    //         Flight::json(
+    //             array(
+    //                 'success' => false,
+    //                 'message' => $is_success
+    //             ), 500
+    //         );
+    //         return;
+    //     }
+
+    //     else {
+    //         Flight::json(
+    //             array(
+    //                 'success' => false,
+    //                 'message' => 'chapter not found'
+    //             ), 404
+    //         );
+    //     }
+    // }
+
+    public function update_chapter_by_id($id_chapter, $id_user)
+    {
+        // catch the query string request
+        $req = Flight::request();
+        $id_folder = $req->data->id_folder;
+        $readed_tiems = $req->data->readed_tiems;
+
+        // initiate the column and values to update
+        $keyValueToUpdate = array();
+
+        // conditional id_folder
+        $valid_id_folder = !is_null($id_folder) && !empty($id_folder) && is_string($id_folder);
+        if ($valid_id_folder) $keyValueToUpdate["id_folder"] = $id_folder;
+
+        // conditional $readed_tiems
+        $valid_readed_tiems = !is_null($readed_tiems) && !empty($readed_tiems) && is_numeric($readed_tiems);
+        if ($valid_readed_tiems) $keyValueToUpdate["readed_tiems"] = $readed_tiems;
+
+        $is_oke_to_update = count($keyValueToUpdate) > 0;
+
+        if ($is_oke_to_update) {
+
+            $result = $this->memverses_chapter->update_chapter_by_id($keyValueToUpdate, $id_user, $id_chapter);
+
+            $is_success = $this->memverses_chapter->is_success;
+
+            if ($is_success === true && $result > 0) {
+                Flight::json(
+                    array(
+                        'success' => true,
+                        'message' => 'Update chapter success',
+                    )
+                );
+            } else if ($is_success !== true) {
+                Flight::json(
+                    array(
+                        'success' => false,
+                        'message' => $is_success
+                    ),
+                    500
+                );
+            } else {
+                Flight::json(
+                    array(
+                        'success' => false,
+                        'message' => 'chapter not found'
+                    ),
+                    404
+                );
+            }
+        } else {
+            Flight::json(
+                array(
+                    'success' => false,
+                    'message' => 'Failed to update chapter, check the data you sent'
+                )
+            );
+        }
+    }
+}
---
+++ app/Memverses/chapters/chapter_model.php
@@ -0,0 +1,126 @@
+<?php
+require_once(__DIR__ . '/../../../utils/database.php');
+
+class Memverses_chapter_model
+{
+    protected $database;
+    var $table = "memverses_chapter";
+    var $is_success = true;
+
+    function __construct()
+    {
+
+        $this->database = Query_builder::getInstance();
+    }
+
+    public function append_chapter($id_chapter_client, $id_user, $chapter, $verse, $readed_times)
+    {
+
+        $data_to_insert = array(
+            'id_chapter_client' => $id_chapter_client,
+            'id_user' => $id_user,
+            'chapter' => $chapter,
+            'verse' => $verse,
+            'readed_times' => $readed_times
+        );
+
+        $this->database->insert($this->table, $data_to_insert);
+
+        if ($this->database->is_error === null) {
+
+            return $this->database->getMaxId($this->table);
+        }
+
+        $this->is_success = $this->database->is_error;
+    }
+
+    public function get_chapters($id_user, $id_folder)
+    {
+        $where_s = array(
+            'id_user' => $id_user,
+            'id_folder' => $id_folder
+        );
+        $result = $this->database->select_where_s($this->table, $where_s)->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($this->database->is_error === null && count($result) > 0) {
+
+            $convert_data_type_chapters = $this->convert_data_type($result);
+
+            return $convert_data_type_chapters;
+        }
+
+        $this->is_success = $this->database->is_error;
+    }
+
+    // public function get_chapter_by_id($id_user, $id_chapter)
+    // {
+
+    //     $where_s = array(
+    //         'id_user' => $id_user,
+    //         'id_chapter' => $id_chapter
+    //     );
+
+    //     $retrieve_chapter = $this->database->select_where_s($this->table, $where_s)->fetchAll(PDO::FETCH_ASSOC);
+
+    //     if ($this->database->is_error === null && count($retrieve_chapter) > 0) {
+
+    //         $convert_data_type = $this->convert_data_type($retrieve_chapter);
+
+    //         return $convert_data_type[0];
+    //     }
+
+    //     $this->is_success = $this->database->is_error;
+    //     return array();
+    // }
+
+    public function update_chapter_by_id(array $data, $id_user, $id_chapter)
+    {
+
+        $where_s = array(
+            'id_user' => $id_user,
+            'id_chapter' => $id_chapter
+        );
+
+        $result = $this->database->update_where_s($this->table, $data, $where_s);
+
+        if ($this->database->is_error === null) return $result;
+
+
+        $this->is_success = $this->database->is_error;
+    }
+
+    // public function remove_chapter_by_id($id)
+    // {
+    //     $result = $this->database->delete($this->table, 'id', $id);
+
+    //     if ($this->database->is_error === null) {
+
+    //         return $result;
+    //     }
+
+    //     $this->is_success = $this->database->is_error;
+    // }
+
+    private function convert_data_type($chapters)
+    {
+
+        $result = array();
+        // mapping chapters
+        foreach ($chapters as $chapter_value) {
+
+            $array_to_push = array(
+                "id" => $chapter_value['id'],
+                "id_chapter" => $chapter_value['id_chapter'],
+                "id_chapter_client" => $chapter_value['id_chapter_client'],
+                "id_folder" => $chapter_value['id_folder'],
+                "chapter" => (int)$chapter_value['chapter'],
+                "verse" => (int)$chapter_value['verse'],
+                "readed_times" => (int)$chapter_value['readed_times']
+            );
+
+            array_push($result, $array_to_push);
+        }
+
+        return $result;
+    }
+}
