21ec331ae0e33ac6709f6e11a48a2276443cd946
rijal <jeryzal333@gmail.com>
2024-11-14 21:02:13 +0700

    Append data to spreadsheet

--- app/google/route.php
+++ app/google/route.php
@@ -4,3 +4,9 @@
 Flight::route('GET /google/sign', function () {
     include('signIn.php');
 });
+
+Flight::route('GET /google/sheet', function () {
+    // 
+    // protected $paymentSpreadsheetId = PAYMENT_SPREADSHEET_ID;
+    include('spreadsheet.php');
+});
---
+++ app/google/spreadsheet.php
@@ -0,0 +1,40 @@
+<?php
+
+use Google\Client;
+use Google\Service\Sheets;
+
+class Google_sheet_operation
+{
+
+    protected $client;
+    protected $service;
+
+    function __construct()
+    {
+        $this->client = new Google_Client();
+        $this->client->setAuthConfig(__DIR__ . '/../../google-config.json');
+        $this->client->setApplicationName('Spreadsheet');
+        $this->client->setScopes(Google_Service_Sheets::SPREADSHEETS);
+
+        $this->service = new Sheets($this->client);
+    }
+
+    function append_data_to_spreadsheet($spreadsheetId, $range, $values)
+    {
+        // checck $spreadsheetId, $$range, values
+        // $values must be array = [
+        //     [
+        //         'A1',
+        //         'B1',
+        //         'C1'
+        //     ]
+        // ];
+
+        $body = new Google_Service_Sheets_ValueRange(['values' => $values]);
+        $params = ['valueInputOption' => 'RAW'];
+        $result = $this->service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+        if ($result) return true;
+        else return false;
+    }
+}
